2ffd06edc78624244218a6b9d90079b82b9327d8
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2020-12-21 21:23:43 +0200

    Added class2 (second part of home work) and updated class1

---
+++ MyDelegate/Class2.cs
@@ -0,0 +1,46 @@
+﻿namespace MyDelegate
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// In that class we describe Method Calc and Result.
+    /// </summary>
+    public class Class2
+    {
+        private int calcResult;
+
+        /// <summary>
+        /// delegate for Result.
+        /// </summary>
+        /// <param name="value">devider.</param>
+        /// <returns>bool value :in divided by value.</returns>
+        public delegate bool ResultHendler(int value);
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pow">Delegate to Class1.Pow</param>
+        /// <param name="x">value for Class1.Pow</param>
+        /// <param name="y">value for Class1.Pow</param>
+        /// <returns>delegate for Result.</returns>
+        public ResultHendler Calc(PowHendler pow, int x, int y)
+        {
+            ResultHendler hendler = Result;
+            this.calcResult = pow.Invoke(x, y);
+            return hendler;
+        }
+
+        /// <summary>
+        /// Is number divided by value.
+        /// </summary>
+        /// <param name="x">divider.</param>
+        /// <returns>bool value :in divided by value.</returns>
+        public bool Result(int x)
+        {
+            return this.calcResult % x == 0 ? true : false;
+        }
+    }
+}
