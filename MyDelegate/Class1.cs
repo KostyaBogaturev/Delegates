157631bb2a48ff774cad0ab969c396cff7c764ae
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2020-12-21 20:46:19 +0200

    Added new class "Class1". That class for first part of hw.

---
+++ MyDelegate/Class1.cs
@@ -0,0 +1,26 @@
+﻿namespace MyDelegate
+{
+
+    /// <summary>
+    /// Delegate for method Show.
+    /// </summary>
+    /// <param name="divisible">Check: is value divisible.</param>
+    public delegate void ShowDelegate(bool divisible);
+    
+    /// <summary>
+    /// In this class we describe method pow.
+    /// </summary>
+    public class Class1
+    {
+        /// <summary>
+        /// This method multiplies two numbers.
+        /// </summary>
+        /// <param name="x">first value.</param>
+        /// <param name="y">seond value.</param>
+        /// <returns>first value * second value.</returns>
+        public int Pow(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
