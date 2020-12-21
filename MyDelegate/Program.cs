6d15e53750da7cb1b49b89ef3bf4346d4590a76b
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2020-12-21 21:34:36 +0200

    Updated class Program.cs

--- MyDelegate/Program.cs
+++ MyDelegate/Program.cs
@@ -1,12 +1,31 @@
-﻿using System;
-
-namespace MyDelegate
+﻿namespace MyDelegate
 {
+    using System;
+
+    /// <summary>
+    /// Main class in program.
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Main method in program.
+        /// </summary>
+        /// <param name="args">standart argument.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var first = new Class1();
+            var second = new Class2();
+            first.showHendler = Show;
+            first.showHendler(second.Calc(first.powHendler, 2, 3).Invoke(2));
+        }
+
+        /// <summary>
+        /// Show method.
+        /// </summary>
+        /// <param name="divider">bool value is number divided or not.</param>
+        private static void Show(bool isDivided)
+        {
+            Console.WriteLine("{0}", isDivided);
         }
     }
 }
